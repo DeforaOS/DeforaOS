6db5c01c5fe60173d8fb40d143bd5890bbc1dab8
Pierre Pronchery <khorben@defora.org>
2013-01-07 02:03:28 +0100

    Preparing scripts for the "clean" target

--- Apps/Network/src/script.sh
+++ Apps/Network/src/script.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
+#$Id$
+#Copyright (c) 2012-2013 Pierre Pronchery <khorben@defora.org>
+#
+#Redistribution and use in source and binary forms, with or without
+#modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+#FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 
@@ -88,16 +110,27 @@ _target_patch()
 #usage
 _usage()
 {
-	echo "Usage: script.sh build|download|extract|package" 1>&2
+	echo "Usage: script.sh [-c|-i|-u][-O name=value...][-P prefix] target..." 1>&2
+	echo "Available targets:" 1>&2
+	echo "  build" 1>&2
+	echo "  configure" 1>&2
+	echo "  download" 1>&2
+	echo "  extract" 1>&2
+	echo "  package" 1>&2
+	echo "  patch" 1>&2
 	return 1
 }
 
 
 #main
+clean=0
 install=0
 uninstall=0
-while getopts "iO:P:u" name; do
+while getopts "ciO:P:u" name; do
 	case "$name" in
+		c)
+			clean=1
+			;;
 		i)
 			install=1
 			;;
@@ -117,32 +150,38 @@ while getopts "iO:P:u" name; do
 	esac
 done
 shift $((OPTIND - 1))
-
-if [ $# -ne 1 ]; then
+if [ $# -eq 0 ]; then
 	_usage
 	exit $?
 fi
-case "$1" in
-	all|clean|distclean|install|uninstall)
-		_target_make "$1"
-		;;
-	build)
-		if [ $uninstall -ne 0 ]; then
-			_target_make 'uninstall'
-		elif [ $install -ne 0 ]; then
-			_target_make 'install'
-		else
-			_target_make all
-		fi
-		;;
-	configure|download|extract|patch)
-		"_target_$1"
-		;;
-	package)
-		_target_package
-		;;
-	*)
-		_usage
-		exit $?
-		;;
-esac
+
+while [ $# -ne 0 ]; do
+	target="$1"
+	shift
+
+	[ $clean -ne 0 ] && target="clean"
+	case "$target" in
+		all|clean|distclean|install|uninstall)
+			_target_make "$target"
+			;;
+		build)
+			if [ $uninstall -ne 0 ]; then
+				_target_make 'uninstall'
+			elif [ $install -ne 0 ]; then
+				_target_make 'install'
+			else
+				_target_make all
+			fi
+			;;
+		configure|download|extract|patch)
+			"_target_$target"
+			;;
+		package)
+			_target_package
+			;;
+		*)
+			_usage
+			exit $?
+			;;
+	esac
+done
