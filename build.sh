2290299fbd66a294c2a5ad4be4595e77c3d7c848
Pierre Pronchery <khorben@defora.org>
2014-03-12 00:33:16 +0900

    More portable shebang, from 7heo <7heo@mail.com>
    Original commit message:
    « The path /bin/sh for the shebang is more portable
    than /usr/bin/env, according to Sven Mascheck:
    http://in-ulm.de/~mascheck/various/shebang/#env
    and:
    http://in-ulm.de/~mascheck/various/shells/
    
    Additionally, a space after the shebang first two
    characters (#!) is said to be more portable:
    http://in-ulm.de/~mascheck/various/shebang/#blankrequired
    
    I fixed the test script shebang accordingly. »
    
    It doesn't say the presence of a space makes it more portable; it says it was a rumor that could never be proven true, for some completely obsolete releases of UNIX. I did not apply that part.
    
    Thanks!

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env sh
+#!/bin/sh
 #$Id$
-#Copyright (c) 2008-2013 Pierre Pronchery <khorben@defora.org>
+#Copyright (c) 2008-2014 Pierre Pronchery <khorben@defora.org>
 #This file is part of DeforaOS
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
